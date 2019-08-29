73b3ddc69d4eda2c4aa381edfd9d965a0236e2a5
alumno <alumno@LAB00PC00>
2019-08-28 21:26:21 -0300

    Ejercicio clase 3 maximo y minimo sin funciones

--- Clase3/src/Clase3.c
+++ Clase3/src/Clase3.c
@@ -11,7 +11,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int status) {
-	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+
+int sumaDosFloat(float numero1,float numero2,float *resultado);
+
+
+/*float identificaMaximo(float);
+float identificaMinimo(float);  */
+
+
+int main(void)
+{
+
+
+	int i;
+	float numeroA;
+	float numeroB;
+	float resultado;
+	float sumaTotal = 0;
+	float maximo = 0;
+	float minimo = 100;
+
+
+
+	for (i=0; i<3 ;i++)
+{
+	float aux = 0;
+
+	printf("El primer numero es: ");
+	scanf("%f",&numeroA);
+	printf("El segundo numero es: ");
+	scanf("%f",&numeroB);
+
+	sumaDosFloat(numeroA, numeroB, &resultado);
+	sumaTotal = sumaTotal+resultado;
+	printf("El resultado de esta suma es:  %f \n",resultado);
+
+	aux = aux + resultado;
+
+	if (aux>maximo)
+		{
+			maximo = aux;
+		}
+
+	if (aux<minimo)
+		{
+			minimo = aux;
+		}
+
+
+	}
+
+	printf("La resultado maximo es:  %f \n",maximo);
+	printf("La resultado minimo es:  %f \n",minimo);
+	printf("La suma total de todas las sumas es:  %f \n",sumaTotal);
+
+
 	return 0;
 }
+
+
+
+int sumaDosFloat(float numero1,float numero2,float *resultado)
+{
+
+		*resultado=numero1+numero2;
+		return 0;
+
+}
+
+
+
+/*
+float identificaMaximo(float* )
+{
+
+}
+
+float identificaMinimo(float)
+{
+
+}
+*/
+
