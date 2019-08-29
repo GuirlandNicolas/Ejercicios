a5ce2695122e88966cc73765eb9a54c971be5808
alumno <alumno@LAB00PC00>
2019-08-28 21:59:47 -0300

    Ejercicio clase 3 Con funciones de Mayor, Menor y suma de float

--- Clase3/src/Clase3.c
+++ Clase3/src/Clase3.c
@@ -14,10 +14,9 @@
 
 
 int sumaDosFloat(float numero1,float numero2,float *resultado);
+float identificaMaximo(float auxA,float maximo, float *maximoF);
+float identificaMinimo(float auxB,float minimo, float *minimoF);
 
-
-/*float identificaMaximo(float);
-float identificaMinimo(float);  */
 
 
 int main(void)
@@ -31,12 +30,15 @@ int main(void)
 	float sumaTotal = 0;
 	float maximo = 0;
 	float minimo = 100;
+	float maximoF = 0;
+	float minimoF = 100;
 
 
 
 	for (i=0; i<3 ;i++)
-{
+	{
 	float aux = 0;
+
 
 	printf("El primer numero es: ");
 	scanf("%f",&numeroA);
@@ -45,27 +47,17 @@ int main(void)
 
 	sumaDosFloat(numeroA, numeroB, &resultado);
 	sumaTotal = sumaTotal+resultado;
-	printf("El resultado de esta suma es:  %f \n",resultado);
 
 	aux = aux + resultado;
+	identificaMaximo(aux, maximo , &maximoF);
+	identificaMinimo(aux, minimo , &minimoF);
 
-	if (aux>maximo)
-		{
-			maximo = aux;
-		}
-
-	if (aux<minimo)
-		{
-			minimo = aux;
-		}
-
-
+	printf("El resultado de esta suma es:  %f \n",resultado);
 	}
 
-	printf("La resultado maximo es:  %f \n",maximo);
-	printf("La resultado minimo es:  %f \n",minimo);
+	printf("La resultado maximo es:  %f \n",maximoF);
+	printf("La resultado minimo es:  %f \n",minimoF);
 	printf("La suma total de todas las sumas es:  %f \n",sumaTotal);
-
 
 	return 0;
 }
@@ -73,24 +65,33 @@ int main(void)
 
 
 int sumaDosFloat(float numero1,float numero2,float *resultado)
+	{
+
+	*resultado=numero1+numero2;
+	return 0;
+
+	}
+
+
+
+
+float identificaMaximo(float auxA, float maximo,float  *maximoF)
 {
-
-		*resultado=numero1+numero2;
-		return 0;
-
+	if (auxA > maximo)
+	{
+		*maximoF = auxA;
+	}
+	return 0;
 }
 
 
-
-/*
-float identificaMaximo(float* )
+float identificaMinimo(float auxB, float minimo,float  *minimoF)
 {
-
+	if (auxB < minimo)
+	{
+		*minimoF = auxB;
+	}
+	return 0;
 }
 
-float identificaMinimo(float)
-{
-
-}
-*/
 
